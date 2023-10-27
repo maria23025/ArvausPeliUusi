3fec23291bdbeded755a7f7f683e6eef100b66c8
Maria Huhtala <maria23025@student.hamk.fi>
2023-10-27 15:18:11 +0300

    Initial commit

---
+++ src/arvauspeli.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class arvauspeliharjoitus {
+
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		String oikeaNimi = "Marko";
+		String arvaus;
+		
+		while (true)
+		{
+			System.out.println ("Arvaa nimi");
+			arvaus = in.nextLine();
+		
+			
+			if (arvaus.equals(oikeaNimi))
+			{
+				System.out.println ("Onneksi olkoon, arvasit oikein!");
+			break;
+		}
+			else 
+			{
+				System.out.println ("Arvasit v‰‰rin, yrit‰ uudestaan.");
+				System.out.println ("Arvaa nimi");
+				arvaus = in.nextLine();
+				
+				if (arvaus.equals(oikeaNimi))
+				{
+					System.out.println ("Onneksi olkoon, arvasit oikein!");
+			break;
+		}
+	}
+		}
+	}
+}
